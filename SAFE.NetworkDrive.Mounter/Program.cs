14aeb8c85ce2cfd4e73030c2088e24b5c1ecedee
oetyng <oetyng@gmail.com>
2019-03-10 14:53:53 +0100

    Code cleanup.

--- SAFE.NetworkDrive.Mounter/Program.cs
+++ SAFE.NetworkDrive.Mounter/Program.cs
@@ -25,15 +25,12 @@ SOFTWARE.
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using DokanNet;
 using Microsoft.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Configuration;
 using NLog;
-using SAFE.NetworkDrive.Interface;
 using SAFE.NetworkDrive.Parameters;
 using SAFE.NetworkDrive.Mounter.Config;
 
@@ -67,14 +64,14 @@ namespace SAFE.NetworkDrive.Mounter
         {
             try
             {
-                if (args.Length > 0)
-                    new Program().ParseCommandLine(args);
-                else
-                {
+                //if (args.Length > 0)
+                //    new Program().ParseCommandLine(args);
+                //else
+                //{
                     var console = new ConsoleApp();
                     var user = console.GetUserConfig();
                     new Program().Mount(user);
-                }
+                //}
             }
             catch(Exception ex)
             {
@@ -82,43 +79,6 @@ namespace SAFE.NetworkDrive.Mounter
             }
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
-        }
-
-        void ParseCommandLine(string[] args)
-        {
-            var commandLine = new CommandLineApplication()
-            {
-                Name = "Mounter",
-                FullName = "SAFE.NetworkDrive.Mounter",
-                Description = "A mount manager for cloud drives",
-                ShortVersionGetter = () => GetFileVersion(typeof(Program).Assembly, 3),
-                LongVersionGetter = () => GetFileVersion(typeof(Program).Assembly, 4)
-            };
-            commandLine.HelpOption("-?|-h|--help");
-
-            commandLine.Command("mount", c =>
-            {
-                var userNames = c.Argument("<userNames>", "If specified, mount the drives associated with the specified users; otherwise, mount all configured drives.", true);
-                var passPhrase = c.Option("-p|--passPhrase", "The pass phrase used to encrypt persisted user credentials and access tokens", CommandOptionType.SingleValue);
-                c.HelpOption("-?|-h|--help");
-                c.OnExecute(() => Mount(passPhrase.Value(), userNames.Values));
-            });
-
-            commandLine.Command("reset", c =>
-            {
-                var userNames = c.Argument("<userNames>", "If specified, purge the persisted settings of the drives associated with the specified users; otherwise, purge the persisted settings of all configured drives.", true);
-                c.HelpOption("-?|-h|--help");
-                c.OnExecute(() => Reset(userNames.Values));
-            });
-
-            commandLine.Execute(args);
-        }
-
-        string GetFileVersion(Assembly assembly, int components)
-        {
-            var fileVersion = (string)assembly.CustomAttributes.Single(c => c.AttributeType == typeof(AssemblyFileVersionAttribute)).ConstructorArguments[0].Value;
-            var versionComponents = fileVersion.Split('.');
-            return string.Join(".", versionComponents.Take(components).ToArray());
         }
 
         CloudDriveFactory GetCloudDriveFactory()
@@ -174,13 +134,13 @@ namespace SAFE.NetworkDrive.Mounter
                             _logger.Info($"Drive '{drive.DisplayRoot}' mounted successfully.");
                         }
 
-                        Console.WriteLine("Press CTRL-BACKSPACE to clear log, any other key to unmount drives");
+                        Console.WriteLine("Press CTRL-BACKSPACE to clear log, 'U' key to unmount drives");
                         while (true)
                         {
                             var keyInfo = Console.ReadKey(true);
                             if (keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control) && keyInfo.Key == ConsoleKey.Backspace)
                                 Console.Clear();
-                            else
+                            else if (keyInfo.Key == ConsoleKey.U)
                                 break;
                         }
 
@@ -203,108 +163,41 @@ namespace SAFE.NetworkDrive.Mounter
             }
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        int Mount(string passPhrase, IList<string> userNames)
-        {
-            var mountSection = _config.GetSection(MountSection.Name).Get<MountSection>();
-            if (mountSection == null)
-                throw new ArgumentNullException("Mount configuration missing");// ConfigurationErrorsException("Mount configuration missing");
+        //void ParseCommandLine(string[] args)
+        //{
+        //    var commandLine = new CommandLineApplication()
+        //    {
+        //        Name = "Mounter",
+        //        FullName = "SAFE.NetworkDrive.Mounter",
+        //        Description = "A mount manager for cloud drives",
+        //        ShortVersionGetter = () => GetFileVersion(typeof(Program).Assembly, 3),
+        //        LongVersionGetter = () => GetFileVersion(typeof(Program).Assembly, 4)
+        //    };
+        //    commandLine.HelpOption("-?|-h|--help");
 
-            try
-            {
-                using (var logFactory = new LogFactory())
-                {
-                    _logger = logFactory.GetCurrentClassLogger();
-                    var factory = GetCloudDriveFactory(); // mountSection.LibPath
-                    using (var tokenSource = new CancellationTokenSource())
-                    {
-                        var tasks = new List<Task>();
-                        foreach (var driveElement in mountSection.Drives.Where(d => !userNames.Any() || userNames.Contains(d.UserName)))
-                        {
-                            var drive = factory.CreateCloudDrive(
-                                driveElement.Schema, 
-                                driveElement.UserName, 
-                                driveElement.Root, 
-                                new CloudDriveParameters()
-                                {
-                                    ApiKey = driveElement.ApiKey,
-                                    EncryptionKey = driveElement.EncryptionKey,
-                                    Parameters = driveElement.GetParameters()
-                                }
-                            );
+        //    commandLine.Command("mount", c =>
+        //    {
+        //        var userNames = c.Argument("<userNames>", "If specified, mount the drives associated with the specified users; otherwise, mount all configured drives.", true);
+        //        var passPhrase = c.Option("-p|--passPhrase", "The pass phrase used to encrypt persisted user credentials and access tokens", CommandOptionType.SingleValue);
+        //        c.HelpOption("-?|-h|--help");
+        //        c.OnExecute(() => Mount(passPhrase.Value(), userNames.Values));
+        //    });
 
-                            if (!drive.TryAuthenticate())
-                            {
-                                var displayRoot = drive.DisplayRoot;
-                                drive.Dispose();
-                                _logger.Warn($"Authentication failed for drive '{displayRoot}'");
-                                continue;
-                            }
+        //    commandLine.Command("reset", c =>
+        //    {
+        //        var userNames = c.Argument("<userNames>", "If specified, purge the persisted settings of the drives associated with the specified users; otherwise, purge the persisted settings of all configured drives.", true);
+        //        c.HelpOption("-?|-h|--help");
+        //        c.OnExecute(() => Reset(userNames.Values));
+        //    });
 
-                            var operations = new CloudOperations(drive, _logger);
+        //    commandLine.Execute(args);
+        //}
 
-                            // HACK: handle non-unique parameter set of DokanOperations.Mount() by explicitely specifying AllocationUnitSize and SectorSize
-                            tasks.Add(Task.Run(() => operations.Mount(driveElement.Root, DokanOptions.NetworkDrive | DokanOptions.MountManager | DokanOptions.CurrentSession, mountSection.Threads, 121, TimeSpan.FromSeconds(driveElement.Timeout != 0 ? driveElement.Timeout : 20), null, 512, 512), tokenSource.Token));
-
-                            var driveInfo = new DriveInfo(driveElement.Root);
-                            while (!driveInfo.IsReady)
-                                Thread.Sleep(10);
-                            _logger.Info($"Drive '{drive.DisplayRoot}' mounted successfully.");
-                        }
-
-                        Console.WriteLine("Press CTRL-BACKSPACE to clear log, any other key to unmount drives");
-                        while (true)
-                        {
-                            var keyInfo = Console.ReadKey(true);
-                            if (keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control) && keyInfo.Key == ConsoleKey.Backspace)
-                                Console.Clear();
-                            else
-                                break;
-                        }
-
-                        tokenSource.Cancel();
-
-                        return 0;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine($"{ex.GetType().Name}: {ex.Message}");
-                return -1;
-            }
-            finally
-            {
-                foreach (var driveElement in mountSection.Drives.Cast<DriveElement>())
-                    Dokan.Unmount(driveElement.Root[0]);
-                //UIThread.Shutdown();
-            }
-        }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        int Reset(IList<string> userNames)
-        {
-            var mountSection = _config.GetSection(MountSection.Name).Get<MountSection>();
-            if (mountSection == null)
-                throw new ArgumentNullException("Mount configuration missing");// ConfigurationErrorsException("Mount configuration missing");
-
-            var factory = GetCloudDriveFactory(); // mountSection.LibPath
-
-            try
-            {
-                foreach (var driveElement in mountSection.Drives.Where(d => !userNames.Any() || userNames.Contains(d.UserName)))
-                {
-                    using (var drive = factory.CreateCloudDrive(driveElement.Schema, driveElement.UserName, driveElement.Root, new CloudDriveParameters()))
-                        drive.PersistSettings?.PurgeSettings(new RootName(driveElement.Schema, driveElement.UserName, driveElement.Root));
-                }
-
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine($"{ex.GetType().Name}: {ex.Message}");
-                return -1;
-            }
-        }
+        //string GetFileVersion(Assembly assembly, int components)
+        //{
+        //    var fileVersion = (string)assembly.CustomAttributes.Single(c => c.AttributeType == typeof(AssemblyFileVersionAttribute)).ConstructorArguments[0].Value;
+        //    var versionComponents = fileVersion.Split('.');
+        //    return string.Join(".", versionComponents.Take(components).ToArray());
+        //}
     }
 }
